80c51387e9872dd852a79f16424be4bb8530a010
Jassahibsingh <95442827+Jassahibsingh@users.noreply.github.com>
2023-12-06 01:34:27 +0530

    update

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -7,16 +7,63 @@ import Link from "next/link";
 import { supabase } from "@/supabase/supabase";
 import { useRouter } from "next/navigation";
 
+interface ProductArray {
+  productid: number;
+  productimage: string;
+  rating: number;
+  price: number;
+  about: string;
+  instock: boolean;
+  heading: string;
+  quantity?: number;
+}
+
+interface ProductData {
+  productID: string;
+  quantity: number;
+}
+
 function Cart() {
   const Router = useRouter();
-  const [cartEmpty, setCartEmpty] = useState<boolean>(false);
+  const [userCartData, setUserCartData] = useState<ProductArray[]>([]);
+  const [totalPrice, setTotalPrice] = useState<string>();
 
   async function cartDataFetch() {
-    const { data: cartData, error: Error } = await supabase
-      .from("user_cart")
-      .select("products")
-      .eq("users", sessionStorage.getItem("userEmail"));
-    // cartData[0].products.
+    try {
+      const { data: cartData, error: Error } = await supabase
+        .from("user_cart")
+        .select("products")
+        .eq("users", sessionStorage.getItem("userEmail"));
+
+      if (cartData && cartData[0]?.products) {
+        let totalPrice = 0;
+        const productPromises = cartData[0].products.map(
+          async (product: ProductData) => {
+            const { data: productData, error: ProductError } = await supabase
+              .from("products")
+              .select("*")
+              .eq("productid", product.productID);
+
+            if (ProductError) {
+              console.log("Error while fetching products data", ProductError);
+            }
+
+            if (productData && productData.length > 0) {
+              productData[0]["quantity"] = product.quantity;
+              totalPrice += productData[0].price * product.quantity;
+              return productData[0];
+            }
+          }
+        );
+
+        const productsArray = await Promise.all(productPromises);
+
+        setUserCartData(productsArray);
+        setTotalPrice(totalPrice.toFixed(2));
+      }
+    } catch (error) {
+      console.error("Error fetching cart data", error);
+    }
   }
 
   useEffect(() => {
@@ -29,87 +76,102 @@ function Cart() {
       <div className="flex">
         <div className="flex flex-col w-full bg-white py-3 px-4 m-3">
           <h1 className="text-[28px] font-medium">
-            {cartEmpty ? "Your Amazon Cart is empty." : "Shopping Cart"}
+            {userCartData.length <= 0
+              ? "Your Amazon Cart is empty."
+              : "Shopping Cart"}
           </h1>
           <span className="my-3">
             <Divider />
           </span>
           <div>
-            {cartEmpty ? (
+            {userCartData.length <= 0 ? (
               "Your Shopping Cart lives to serve. Give it purpose — fill it with groceries, clothing, household supplies, electronics, and more. Continue shopping on the Amazon.com homepage, learn about today's deals, or visit your Wish List."
             ) : (
-              <div className="flex">
-                <img className="w-[150px] mx-5" src="/uno.jpg" alt="" />
-                <div className="flex flex-col w-full text-[18px] space-y-1">
-                  <span className="w-full flex justify-between">
-                    <p>
-                      Mattel Games UNO Card Game, Toy for Kids and Adults,
-                      Family Game for Campi…
-                    </p>
-                    <p className="font-bold">$10.99</p>
-                  </span>
-                  <p className="text-[12px]">In Stock</p>
-                  <p className="text-[12px]">
-                    Gift options not available.
-                    <Link
-                      className="text-[#007185] hover:text-[#f08804] hover:underline cursor-pointer"
-                      href={"#"}
-                    >
-                      {" "}
-                      Learn more
-                    </Link>
-                  </p>
-                  <div className="flex items-center h-10">
-                    <select
-                      className="w-[70px] border bg-[#F0F2F2] border-[#D5D9D9] hover:bg-[#e9e9e9] text-[13px] focus:ring-cyan-400 focus:ring-opacity-100 rounded-lg my-3 py-[5px] px-[7px] outline-none"
-                      style={{ boxShadow: "0 2px 5px 0 rgba(213,217,217,.5)" }}
-                      name="Country"
-                      id=""
-                      defaultValue={1}
-                    >
-                      <option className="" value="" disabled hidden>
-                        Qty:1
-                      </option>
-                      {Array.from({ length: 30 }, (_, i) => (
-                        <option key={i} value={i + 1}>
-                          {i + 1}
-                        </option>
-                      ))}
-                    </select>
-                    <span className="mx-4 h-[15px]">
-                      <Divider orientation="vertical" />
-                    </span>
-                    <Link
-                      className="text-[#007185] text-[12px] hover:text-[#f08804] hover:underline cursor-pointer"
-                      href={"#"}
-                    >
-                      {" "}
-                      Delete
-                    </Link>
-                    <span className="mx-4 h-[15px]">
-                      <Divider orientation="vertical" />
-                    </span>
-                    <Link
-                      className="text-[#007185] text-[12px] hover:text-[#f08804] hover:underline cursor-pointer"
-                      href={"#"}
-                    >
-                      {" "}
-                      Save for Later
-                    </Link>
+              <div className="flex flex-col mb-3">
+                {userCartData.map((product: ProductArray, id: number) => (
+                  <div className="flex mb-10" key={id}>
+                    <img
+                      className="w-[150px] mx-5"
+                      src={product.productimage}
+                      alt=""
+                    />
+                    <div className="flex flex-col w-full text-[18px] space-y-1">
+                      <span className="w-full flex justify-between">
+                        <p>
+                          {product.heading.length > 140
+                            ? product.heading.substring(0, 140) + "..."
+                            : product.heading}
+                        </p>
+                        <p className="font-bold ml-10">${product.price}</p>
+                      </span>
+                      <p className="text-[12px]">{product.instock}</p>
+                      <p className="text-[12px]">
+                        Gift options not available.
+                        <Link
+                          className="text-[#007185] hover:text-[#f08804] hover:underline cursor-pointer"
+                          href={"#"}
+                        >
+                          {" "}
+                          Learn more
+                        </Link>
+                      </p>
+                      <div className="flex items-center h-10">
+                        <select
+                          className="w-[70px] border bg-[#F0F2F2] border-[#D5D9D9] hover:bg-[#e9e9e9] text-[13px] focus:ring-cyan-400 focus:ring-opacity-100 rounded-lg my-3 py-[5px] px-[7px] outline-none"
+                          style={{
+                            boxShadow: "0 2px 5px 0 rgba(213,217,217,.5)",
+                          }}
+                          name="Country"
+                          id=""
+                          defaultValue={1}
+                          value={product.quantity}
+                        >
+                          {Array.from({ length: 30 }, (_, i) => (
+                            <option key={i} value={i + 1}>
+                              {i + 1}
+                            </option>
+                          ))}
+                        </select>
+                        <span className="mx-4 h-[15px]">
+                          <Divider orientation="vertical" />
+                        </span>
+                        <Link
+                          className="text-[#007185] text-[12px] hover:text-[#f08804] hover:underline cursor-pointer"
+                          href={"#"}
+                        >
+                          {" "}
+                          Delete
+                        </Link>
+                        <span className="mx-4 h-[15px]">
+                          <Divider orientation="vertical" />
+                        </span>
+                        <Link
+                          className="text-[#007185] text-[12px] hover:text-[#f08804] hover:underline cursor-pointer"
+                          href={"#"}
+                        >
+                          {" "}
+                          Save for Later
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                ))}
               </div>
             )}
-            <Divider className="my-3" />
-            <div className="flex w-full justify-end text-[18px]">
-              Subtotal (1 item): <p className="font-bold ml-2"> $10.99</p>
+            <span className="my-3">
+              <Divider />
+            </span>
+            <div className="flex w-full justify-end text-[18px] mt-3">
+              Subtotal ({userCartData.length} item):{" "}
+              <p className="font-bold ml-2"> ${totalPrice}</p>
             </div>
           </div>
         </div>
         <div className="flex flex-col w-[400px]">
           <div className="flex flex-col items-center justify-between h-[110px] bg-white py-3 px-4 m-3">
             <div className="flex w-full justify-start text-[18px]">
-              Subtotal (1 item): <p className="font-bold ml-2"> $10.99</p>
+              Subtotal ({userCartData.length} item):{" "}
+              <p className="font-bold ml-2"> ${totalPrice}</p>
             </div>
             <span
               onClick={() => Router.push("/checkout")}
