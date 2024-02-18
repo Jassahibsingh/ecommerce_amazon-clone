2f2ab98a4dd46dd388a434dd9db6a79e7aac694b
Jassahibsingh <95442827+Jassahibsingh@users.noreply.github.com>
2024-02-18 17:33:54 +0530

    update

---
+++ src/app/search/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../header";
+import Footer from "../footer";
+
+function Search() {
+  return (
+    <div>
+      <Header />
+      Search
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
